a1803754a6d5904a59811be96ea4f40e4bb1621e
EnzeXu <xue20@wfu.edu>
2022-09-13 16:29:47 -0400

    updates

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 import os
 import warnings
+import argparse
 
 from translator import detector, translator, create_pdf_pages, build_pdf
 
 warnings.filterwarnings("ignore")
 
 
-def run(path):
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", "-p", type=str, default="files/test.pdf", help="file path")
+    opt = parser.parse_args()
+    path = opt.path
+    if not os.path.exists(path):
+        path = "{}/{}".format("files", path)
+        if not os.path.exists(path):
+            print("File does not exists! Please check the file path.")
+            return
+
     filename = path.split("/")[-1].replace(".pdf", "").replace(".PDF", "")
     detected_path = "saves/{}_detected.pkl".format(filename)
     if os.path.exists(detected_path):
@@ -31,4 +42,4 @@ def run(path):
 
 
 if __name__ == "__main__":
-    run("files/Manuscript_V4.pdf")
+    run()
